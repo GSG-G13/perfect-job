b7408ad7a0c651755627faba8c0fa08de2b53b37
heleena-fadi <miss.heleena98@gmail.com>
2023-05-17 22:19:50 +0300

    fixing

--- src/components/JobCard/index.jsx
+++ src/components/JobCard/index.jsx
@@ -9,10 +9,11 @@ import './style.css'
          <img src={job.employer_logo || 'https://cdn-payscale.com/content/placeholder-images/job-placeholder.png'} alt={job.job_title} className='card-logo'
         />
         <div className='card-content'>
-          {job.employer_name}
-          {job.job_city}
-          {/* {job.job_description} */}
-
+         <h4 className='card-header'> {job.employer_name}</h4>
+          <p className='text-header'>{job.job_city}</p>
+          <div className='skills'>{job.job_required_skills}/</div>
+          <div className='position'>{job.job_employment_type}</div>
+          <span className='timeStamp'>{job.job_posted_at_timestamp}</span>
         </div>
       </div>
     )
